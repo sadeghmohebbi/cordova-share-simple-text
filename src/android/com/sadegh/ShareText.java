8bad3f109a847d83b044166054d1cd8b028c41eb
sadeghdev <mohebbi.sadegh@gmail.com>
2017-06-04 16:08:18 +0430

    java class cleaned up

--- src/android/com/sadegh/ShareText.java
+++ src/android/com/sadegh/ShareText.java
@@ -1,4 +1,5 @@
 /**
+ *created by sadegh mohebbi
  */
 package com.sadegh;
 
@@ -6,49 +7,46 @@ import org.apache.cordova.CallbackContext;
 import org.apache.cordova.CordovaInterface;
 import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.CordovaWebView;
-import org.apache.cordova.PluginResult;
-import org.apache.cordova.PluginResult.Status;
-import org.json.JSONObject;
 import org.json.JSONArray;
 import org.json.JSONException;
 
+import android.content.Context;
+import android.content.Intent;
 import android.util.Log;
 
-import java.util.Date;
 
 public class ShareText extends CordovaPlugin {
-  private static final String KEY_SHARE = "share";
-  private static final String TAG = "ShareText";
+    private static final String KEY_SHARE = "share";
+    private static final String TAG = "ShareText";
 
-  public ShareText() {}
+    public ShareText() {}
 
-  public void initialize(CordovaInterface cordova, CordovaWebView webView) {
-    super.initialize(cordova, webView);
+    public void initialize(CordovaInterface cordova, CordovaWebView webView) {
+        super.initialize(cordova, webView);
 
-    Log.d(TAG, "Initializing ShareText Plugin");
-  }
-
-  public boolean execute(String action, JSONArray args, final CallbackContext callbackContext) throws JSONException {
-    if(action.equals(KEY_SHARE)) {
-      try {
-        String phrase = args.getString(0);
-        Log.d(TAG, phrase);
-      } catch(JSONException e) {
-        Log.i(TAG, e.getLocalizedMessage());
-        continue;
-      }
-
-      try {
-        Intent intent = new Intent(Intent.ACTION_SEND);
-        intent.setType("text/plain");
-        goToOffline.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TOP);
-        intent.putExtra(Intent.EXTRA_TEXT, phrase);
-        intent.setPackage(this.cordova.getActivity().getApplicationContext().getPackageName());
-        this.cordova.startActivity(Intent.createChooser(intent, "Share this text via"));
-      } catch (Exception e) {
-        Log.i(TAG, e.getLocalizedMessage())
-      }
+        Log.d(TAG, "Initializing ShareText Plugin");
     }
-    return true;
-  }
+
+    public boolean execute(String action, JSONArray args, final CallbackContext callbackContext) throws JSONException {
+        String phrase = null;
+        if(action.equals(KEY_SHARE)) {
+            phrase = args.getString(0);
+            Log.d(TAG, phrase);
+
+            if (phrase != null) {
+                try {
+                    Context context = this.cordova.getActivity().getApplicationContext();
+                    Intent intent = new Intent(Intent.ACTION_SEND);
+                    intent.setType("text/plain");
+                    intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                    intent.putExtra(Intent.EXTRA_TEXT, phrase);
+                    intent.setPackage(this.cordova.getActivity().getApplicationContext().getPackageName());
+                    context.startActivity(Intent.createChooser(intent, "Share this text via"));
+                } catch (Exception e) {
+                    Log.i(TAG, e.getLocalizedMessage());
+                }
+            }
+        }
+        return true;
+    }
 }
